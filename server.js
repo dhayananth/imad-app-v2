4f9693654c8c8c2f8be1baca113398d5568ba43a
dhayananth <dhaya635@gmail.com>
2017-02-22 20:27:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,67 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone={
+    title:'article-one dhayananth',
+    heading:'article one',
+    date:'sep 5',
+    content:`
+     <p>
+                ihshefhjewufebfdsihfhoewlknfflkehgegreg
+                regdxfhgjhkgfjgdhfsgdfszdgfngmh,bjkghfutdyrstdfzxvcbvnbmvhkyfutytsdfzxcbvcnbvmhjdyfg
+                gzhdjfkghkhfjgdhfdsgdhfgjhkfutdygdhtuy
+            </p>
+            <p>
+                hiewoufhweujbfhpuiegfsjnjhfoeulwknetewrt
+                er
+                ryetudiytouiydruterysewetsyhfdxdjgkfyirertearsfgzxfgkyir7erygxbfhduss
+                ryeyutriytruteyrydjgfkyiu
+            </p>
+            <p>
+                fnoiwejfnmkewpjffi'wolkev'iwefniperjgpis'e;msv'kiehoghodv
+                rwteyturytikydjtsrsaFSZGdxfncgmvh,vjlhuotiyrtertesdgzbcxncmgch
+                awrteyresutriytyiruteyrwtewrwagdshfdjgkfhgluourtyrsgd
+            </p>`
+};
+
+function createTemplate(data){
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+var htmlTemplate= `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <a href="/">home</a>
+        <hr/>
+        <h1>
+            ${heading}
+        </h1>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createTemplate(articleone));
 });
 app.get('/article-two',function(req,res){
     res.send('article two is requested');
